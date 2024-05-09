b6f8ee4ef3cd755e5c8c3d24689250347d1cec37
Filip Churlevski <filip@skenariolabs.com>
2024-05-09 11:15:46 +0300

    Adding port

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: 'http://localhost:9090/',
+    port: 3000
+  }
 })
